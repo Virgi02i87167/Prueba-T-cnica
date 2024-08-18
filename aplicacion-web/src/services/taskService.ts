0c66fdc50d53edfc95f6238275c9650418b3584c
Virgi02i87167 <131634183+Virgi02i87167@users.noreply.github.com>
2024-08-18 10:40:56 -0600

    funciones de interaccion con la Api

--- aplicacion-web/src/services/taskService.ts
+++ aplicacion-web/src/services/taskService.ts
@@ -10,3 +10,12 @@ export const createTask = async (task: Omit<Task, 'id'>) => {
     const response = await axiosInstance.post('/tasks', task);
     return response.data;
 }
+
+export const updateTask = async (id: number, task: Partial<Omit<Task, 'id'>>) => {
+    const response = await axiosInstance.post(`/tasks/${id}`, task);
+    return response.data;
+}
+
+export const deleteTask = async (id: number) => {
+    await axiosInstance.delete(`/tasks/${id}`);
+}
