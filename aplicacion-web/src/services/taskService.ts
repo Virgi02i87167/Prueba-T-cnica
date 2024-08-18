c623b3e078301a2a054a03a2de504e417428003d
Virgi02i87167 <131634183+Virgi02i87167@users.noreply.github.com>
2024-08-18 12:33:12 -0600

    cambios

--- aplicacion-web/src/services/taskService.ts
+++ aplicacion-web/src/services/taskService.ts
@@ -1,21 +1,21 @@
-import axiosInstance from "@/lib/axiosInstace";
+import axiosInstance from "../lib/axiosInstace";
 import { Task } from '../types';
 
 export const getTasks = async () => {
     const response = await axiosInstance.get('/tasks');
     return response.data;
-}
+};
 
 export const createTask = async (task: Omit<Task, 'id'>) => {
     const response = await axiosInstance.post('/tasks', task);
     return response.data;
-}
+};
 
 export const updateTask = async (id: number, task: Partial<Omit<Task, 'id'>>) => {
-    const response = await axiosInstance.post(`/tasks/${id}`, task);
+    const response = await axiosInstance.put(`/tasks/${id}`, task);
     return response.data;
-}
+};
 
 export const deleteTask = async (id: number) => {
     await axiosInstance.delete(`/tasks/${id}`);
-}
+};
